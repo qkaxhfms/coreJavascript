047f2b3efd78813311da747cc9b49ad7b9590c28
MS\kwakm <kwakms821@daum.net>
2021-02-11 23:56:31 +0900

    연산 실행하기

--- e.calculator/calculator.js
+++ e.calculator/calculator.js
@@ -8,19 +8,46 @@
 
 const calculator = {
     displayValue: "0",
-    // displayValue: "1",
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
 };
 
 function inputDigit(valueEach) {
     console.log("각 항목 값 출력");
 
-    const { displayValue } = calculator;
+    const { displayValue, waitingForSecondOperand } = calculator;
     // 현재 값이 '0'이면 'displayValue'를 덮어쓰거나 여기에 추가
 
-    calculator.displayValue =
-        displayValue === "0" ? valueEach : displayValue + valueEach;
+    if (waitingForSecondOperand === true) {
+        calculator.displayValue = digit;
+        calculator.waitingForSecondOperand = false;
+    } else {
+        calculator.displayValue =
+            displayValue === "0" ? valueEach : displayValue + valueEach;
+    }
+    console.log(calculator);
 }
 
+function inputDemical(dot) {
+    if (!calculator.displayValue.includes(dot)) {
+        //소숫점이 포함되어 있지 않다면
+        calculator.displayValue += dot;
+    }
+}
+//연산자 처리
+function handleOperator(nextOperator) {
+    const { firstOperand, displayValue, operator } = calculator;
+    const inputValue = parseFloat(displayValue); // 문자열 내용을 변환, 부동소수점 번호로
+
+    // firstOperand가 null이고, inputValue가 있는지 확인합니다. , Nan 값이 아닙니다.
+    if (firstOperand == null && !isNaN(inputValue)) {
+        calculator.firstOperand = inputValue;
+    }
+    calculator.waitingForSecondOperand = true;
+    calculator.operator = nextOperator;
+    console.log(calculator);
+}
 // 창 최신화
 function updateDisplay() {
     // console.log("업데이트");
