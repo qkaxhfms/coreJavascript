c6efeb50f2698a418d56921933c96ddfb1223b0d
MS\kwakm <kwakms821@daum.net>
2021-02-14 23:01:07 +0900

    [ CI ] pager 완성

--- e.pager/js/common.js
+++ e.pager/js/common.js
@@ -3,39 +3,52 @@ const btnPrev = document.querySelector(".btnPrev");
 const btnNext = document.querySelector(".btnNext");
 const btnStepAll = document.querySelectorAll(".btnStep");
 
-let currentNum;
+let currentNum = btnStepAll[0];
+currentNum.classList.add("active");
+console.log(currentNum);
 
 const stepNext = () => {
-    console.log("다음으로 이동");
+    // console.log("다음으로 이동");
+    for (let k = 0; k < btnStepAll.length; k++) {
+        btnStepAll[k].classList.remove("active");
+    }
+    if (currentNum.classList.contains("btnStep")) {
+        // if (currentNum) {
+        currentNum.nextElementSibling.classList.add("active");
+        currentNum = currentNum.nextElementSibling;
+        // }
+    }
+    if (btnNext.classList.contains("active")) {
+        btnNext.classList.remove("active");
+    }
 };
 const stepPrev = () => {
-    console.log("이전으로 이동");
+    // console.log("이전으로 이동");
+    for (let k = 0; k < btnStepAll.length; k++) {
+        btnStepAll[k].classList.remove("active");
+    }
+    if (currentNum.classList.contains("btnStep")) {
+        // if (currentNum) {
+        currentNum.previousElementSibling.classList.add("active");
+        currentNum = currentNum.previousElementSibling;
+        // }
+    }
+    if (btnPrev.classList.contains("active")) {
+        btnPrev.classList.remove("active");
+    }
 };
 
 for (let i = 0; i < btnStepAll.length; i++) {
-    const stepCurrent = () => {
+    const stepCurrent = (e) => {
         for (let k = 0; k < btnStepAll.length; k++) {
             btnStepAll[k].classList.remove("active");
         }
         btnStepAll[i].classList.add("active");
+        currentNum = e.target; //현재값
+        console.log(currentNum);
     };
     btnStepAll[i].addEventListener("click", stepCurrent);
 }
 
 btnNext.addEventListener("click", stepNext);
 btnPrev.addEventListener("click", stepPrev);
-
-/*
-
-기본
-1. 버튼을 누른다.
-2. 누른 버튼이 활성화 된다.
-
-다음
-1. 다음 버튼을 누른다.
-2. btnStep에서 순차적으로 활성화된다.
-
-중요
-전 버튼을 기억하고 다음을 활성화 해야함
-
-*/
