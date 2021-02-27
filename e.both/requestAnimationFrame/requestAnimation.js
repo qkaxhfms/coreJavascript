0575262a684919404448d3996504c9a565cdc4ae
MS\kwakm <kwakms821@daum.net>
2021-02-27 21:02:48 +0900

    [ CI ] 움직이는 상자 완성

--- e.both/requestAnimationFrame/requestAnimation.js
+++ e.both/requestAnimationFrame/requestAnimation.js
@@ -3,7 +3,7 @@
 // 움직이는 박스
 const btn = document.querySelector('button');
 const box = document.querySelector('#box');
-let duration = 2000;
+let duration = 500;
 let startTime;
 let num = 0;
 
@@ -11,24 +11,28 @@ btn.onclick = function () {
     // requestAnimationFrame(run);
     // 이벤트 발생 시 시작시간 저장
     startTime = performance.now();
-
     requestAnimationFrame(run);
 };
 function run(time) {
     // 반복된 누적시간(해당 콜백이 반복될때마다의 시간에서 시작 시작을 뺴서 저장)
     let timeLast = time - startTime;
-    timer = requestAnimationFrame(run);
-    num++;
-    if (num >= 100) cancelAnimationFrame(timer);
-    console.log(`누적시간:${timeLast}`);
-    console.log(num);
+
+    // 진행률 (반복된 누적시간을 전체 총 시간으로 나누면 0~1까지 진행률을 환산 가능)
+    let progress = timeLast / duration;
+
+    // 진행률이실행도리 떄 초기값이 음수로 넘억마으로 음수일 때 무조건 0으로 보정
+    if (progress < 0) progress = 0;
+    if (progress > 1) progress = 1;
+
+    // 진행률이 1에 도달하지 않으면 계속해서 requestAnimationFrame으로 run함수 반복
+    // 진행률이 1에 돋라하면 cancleANimationFrame으로 run함수 종료
+
+    if (progress < 1) {
+        timer = requestAnimationFrame(run);
+    } else {
+        cancelAnimationFrame(timer);
+    }
+
+    console.log(`현재까지 걸린 시간 :${timeLast}/진행률:${progress}`);
+    box.style.marginLeft = 450 * progress + 'px';
 }
-
-// function run() {
-//     num++;
-//     console.log(num);
-//     timer = requestAnimationFrame(run);
-
-//     if (num >= 300) cancelAnimationFrame(timer);
-//     box.style.marginLeft = num + 'px';
-// }
