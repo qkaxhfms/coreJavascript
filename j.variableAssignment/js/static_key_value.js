f9a6b7c3c0594bfb7bd173cc1cd2fb0ddb1bf8ee
MS\kwakm <kwakms821@daum.net>
2021-03-01 23:00:50 +0900

    [ CI ] 정적인 정보를 다루기에 유리

---
+++ j.variableAssignment/js/static_key_value.js
@@ -0,0 +1,28 @@
+// 정적인 정보를 다루기에 유리
+// const colors = {
+//     red: '#d10202',
+//     green: '#19d836',
+//     blue: '#0e33d8',
+// };
+
+// export const config = {
+//     endpoint: 'http://pragprog.com',
+//     key: 'secretkey',
+// };
+
+// function getBill(item) {
+//     return {
+//         name: item.name,
+//         due: twoWeeksFromNow(),
+//         totoal: calculateTotal(item.price),
+//     };
+// }
+
+// const bill = getBill({
+//     name: '객실청소',
+//     price: 30,
+// });
+
+// function displayBill(bill) {
+//     return `${bill.name} 비용은 ${bill.total}달러이며 납부일은 ${bill.due}입니다.`;
+// }
