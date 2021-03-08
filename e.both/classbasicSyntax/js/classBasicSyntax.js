7f8f1b498ad09131d1df3e3ddd499ca3e570d6e5
MS\kwakm <kwakms821@daum.net>
2021-03-08 23:59:33 +0900

    [ CI ] 클래스형 종료

--- e.both/classbasicSyntax/js/classBasicSyntax.js
+++ e.both/classbasicSyntax/js/classBasicSyntax.js
@@ -18,5 +18,15 @@ class User {
     }
 }
 
-let user = new User('John');
-user.sayHi();
+// let user = new User('John');
+// user.sayHi();
+
+console.log(typeof User); // function
+
+// 생성자 메서드와 동일
+console.log(User === User.prototype.constructor); //true
+
+// 클래스 내부에서 정의한 메서드는 User.prototype에 저장됩니다.
+console.log(User.prototype.sayHi); //console.log(this.name)
+
+// 클래스는 단순한 편의 문법이 아니다. = 기능이 동일하나 기존 문법을 쉽게 읽을 수 있게 만든 문법
